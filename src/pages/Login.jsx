95c7d082eec54ec287624376495fd4a47136f44a
S'amyk Bajracharya <samykbajr919@gmail.com>
2025-04-10 21:45:21 +0545

    s

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -64,6 +64,13 @@ export default function Login() {
         >
           Login
         </button>
+
+        <p className="mt-4 text-center">
+          Don't have an account?{' '}
+          <a href="/signup" className="text-blue-500 hover:underline">
+            Sign Up
+          </a>
+        </p>
       </form>
     </div>
   );

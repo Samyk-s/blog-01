987e8bebb5fce1044b0f22e4980cda16ae19fd4b
S'amyk Bajracharya <samykbajr919@gmail.com>
2025-04-10 22:00:00 +0545

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Blogs from './pages/Blogs';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Signup from './pages/Signup'; // Assuming Signup.jsx is inside 'pages' folder
+import Login from './pages/Login'; 
+import Blogs from './pages/Blogs'; 
 
 function App() {
-  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/blogs" element={isLoggedIn ? <Blogs /> : <Navigate to="/login" />} />
+        <Route path="/blogs" element={<Blogs />} />
+        {/* Handle other routes */}
       </Routes>
     </Router>
   );

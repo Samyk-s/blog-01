8f3e5687079592f80675ead7f9f825065201c316
S'amyk Bajracharya <samykbajr919@gmail.com>
2025-04-10 22:02:33 +0545

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Signup from './pages/Signup'; // Assuming Signup.jsx is inside 'pages' folder
-import Login from './pages/Login'; 
-import Blogs from './pages/Blogs'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';  // Assuming Signup.jsx is inside 'pages' folder
+import Login from './pages/Login';
+import Blogs from './pages/Blogs';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login />} />  {/* Add root route */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/blogs" element={<Blogs />} />
